bac0f0818d57b22fdeb39b1454b50852b70f6f1e
郑朋亮 <zhengpengliang@100tal.com>
2018-12-19 18:21:04 +0800

    commit

--- src/test/java/com/springbootlearn/damo/ThinkInJavaTests.java
+++ src/test/java/com/springbootlearn/damo/ThinkInJavaTests.java
@@ -1,4 +1,14 @@
 package com.springbootlearn.damo;
 
+
+import java.util.Date;
+
+/**
+ * @author zhengpengliang
+ * @email zhengpengliang@100tal.com
+ */
 public class ThinkInJavaTests {
+    public static void main(String[] args){
+        System.out.println("hello ,it's "+new Date());
+    }
 }
